e52bd5019cbea2b745c6bb15826a856a659b07d0
Ronmacc <154700667+ronmaccms@users.noreply.github.com>
2024-02-04 14:18:15 -0500

    New Project Quiz App

---
+++ QuizApp/src/App.vue
@@ -0,0 +1,85 @@
+<script setup>
+  import q from "./data/quizes.json"
+  import {ref, watch} from "vue"
+
+  const quizes = ref(q)
+  const search = ref("")
+
+  watch(search, () => {
+    quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+  })
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input v-model.trim="search" type="text" placeholder="Search...">
+    </header>
+    <div class="options-container">
+      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img :src="quiz.img" alt="">
+        <h2>{{ quiz.name }}</h2>
+        <p>{{ quiz.questions.length }} questions</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128,128,128,0.1);
+  padding: 1px;
+  border-radius: 5px;
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+
+/* CARD */
+
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
+  margin-bottom: 35;
+  margin-right: 20;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  margin: 0;
+  height: 190px;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
+}
+</style>

f9f1daf54dc7df2ec5409697bb447a6b20df3d73
Ronmacc <154700667+ronmaccms@users.noreply.github.com>
2024-02-08 12:13:21 -0500

    practicing debugging

---
+++ third/scriptDebugTest.js
@@ -0,0 +1,32 @@
+// const users = [
+//     { name: 'Kyle', age:24},
+//     { name: 'Joey', age:24},
+//     { name: 'Bailee', age:22},
+//     { name: 'Jim', age:34},
+//     { name: 'Phil' },
+//     { name: 'Sarah', age:54},
+//     { name: 'John', age:42},
+// ]
+
+// users.forEach(user => {
+//     printUser(user)
+// })
+
+// function printUser(user) {
+//     if (user.age != null) {
+//         console.log(`User ${user.name} will be ${user.age + 5} years old in five years.`)
+//     } else {
+//         console.log(`User is called ${user.name}`)
+//     }    
+// }
+
+
+const button = document.getElementById('btn')
+const input = document.getElementById('textElement')
+
+
+
+button.addEventListener('click', () => {
+    console.log(input.value)
+    debugger;
+})
